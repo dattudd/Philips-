91c2662ecbf3e5def195092d85d35fb392dd3395
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2024-04-05 15:22:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,9 +44,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LOCO-PILOT/ROYMUSIC",
+    "https://github.com/adityaranjan0918/ROYMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "patch-2")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
